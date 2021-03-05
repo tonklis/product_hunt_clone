18d5835b38457de9174bd1080c74efd866437e8f
Benjamin Hernandez <benjamin@coderia.mx>
2021-03-05 10:25:52 -0600

    Automated tests with travis

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,23 @@ class ProductsController < ApplicationController
     @products = Product.all
   end
 
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
+
 end
